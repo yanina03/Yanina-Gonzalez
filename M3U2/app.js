088af66a5157533241b58ee3de7c37033d23cc83
Yanina Gonzalez <yaninagonzalez94@hotmail.com.ar>
2023-01-29 11:26:09 -0300

    M3U2

--- M3U2/app.js
+++ M3U2/app.js
@@ -4,14 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');//routes/index.js
-var galeriaRouter = require('./routes/galeria'); //routes/galeria.js
-var contactosRouter = require('./routes/contactos'); //routes/contactos.js
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,28 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/galeria', galeriaRouter);
-app.use('/contactos', contactosRouter);
-
-
-app.get('/prueba',function(req,res){
-  res.send('Hola soy la pagina de prueba')
-
-})
-
-app.get('/nosotros',function(req,res){
-  res.send('Hola soy la pagina de Nosotros')
-
-})
-
-app.get('/servicios',function(req,res){
-  res.send('Hola soy la pagina de servicios')
-
-})
-
-
-
-
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
